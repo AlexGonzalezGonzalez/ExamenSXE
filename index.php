2163cfa3ded0fe21827dedd1b280c1b9caddd2ec
aljagogo@yahoo.es <ec2-user@ip-172-31-1-23.us-west-2.compute.internal>
2018-12-17 08:46:49 +0000

    index

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div class="wrap">
+	<?php if ( is_home() && ! is_front_page() ) : ?>
+		<header class="page-header">
+			<h1 class="page-title"><?php single_post_title(); ?></h1>
+		</header>
+	<?php else : ?>
+	<header class="page-header">
+		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
+	</header>
+	<?php endif; ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			if ( have_posts() ) :
+				/*Loop`de los post donde creamos una tabla*/
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+				if(in_category('Numero 1')):
+					else:
+				?>	
+					
+					<table>
+						<tr><th><p>Titulo: <?php	the_title(); ?></p></th></tr>
+						<?php
+						if(has_post_thumbnail()):
+							?>
+							<tr><th><?php the_post_thumbnail(); ?></th><th><?php the_excerpt(); ?></th></tr>
+							<?php
+						else:
+							?>
+						<tr><th><?php the_excerpt(); ?></th></tr>
+						<?php
+						endif;
+						?>
+						<tr><th> <a href="<?php the_permalink() ?>">Read More</a> </th></tr>
+						<tr><th><p>Autor: <?php the_author(); ?></p></th><th><?php the_time(); ?></th></tr>
+					</table>	
+					
+				<?php 
+				endif;
+				endwhile;
+
+				the_posts_pagination( array(
+					'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+					'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+				) );
+
+			else :
+
+				get_template_part( 'template-parts/post/content', 'none' );
+
+			endif;
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+	<?php get_sidebar(); ?>
+</div><!-- .wrap -->
+
+<?php get_footer();
